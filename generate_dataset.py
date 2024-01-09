8e72b979abb1d7912c860109eff1ac6ce321632e
lion158 <szczepandpolak@gmail.com>
2024-01-09 17:53:17 +0100

    update dataset script

--- generate_dataset.py
+++ generate_dataset.py
@@ -4,11 +4,12 @@ from pyspark.sql.functions import col
 from pyspark.sql.types import IntegerType
 import random
 
-
-if len(sys.argv) == 1:
-    print("Please provide a GCS bucket name.")
+    
+if len(sys.argv) < 3:
+    print("Please provide both a GCS bucket name and size.")
 
 bucket = sys.argv[1]
+size = sys.argv[2]
 
 # Inicjalizacja sesji Spark
 spark = SparkSession.builder.appName("GenerateRandomDataset").getOrCreate()
@@ -21,7 +22,7 @@ def generate_random_number():
 generate_random_number_udf = spark.udf.register("generate_random_number_udf", generate_random_number, IntegerType())
 
 # Liczba rekordów potrzebna do osiągnięcia 10GB (10^10 bajtów)
-target_size_bytes = 1 * 1024**3
+target_size_bytes = size * 1024**3
 num_records = target_size_bytes // 4  # Zakładamy, że każda liczba to 4 bajty (int)
 
 # Generowanie DataFrame z losowymi liczbami
