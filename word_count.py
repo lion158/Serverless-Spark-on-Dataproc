da0e97c08e53dec4a68db81bd1fd034ba64ec886
lion158 <szczepandpolak@gmail.com>
2024-01-15 21:16:35 +0100

    word_count update

--- word_count.py
+++ word_count.py
@@ -4,6 +4,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split, size, sum
 import time
 
+if len(sys.argv) < 3:
+    print("Please provide both a GCS bucket name and output_file name.")
+
+bucket = sys.argv[1]
+output_file = sys.argv[2]
+
 table = "bigquery-public-data:github_repos.sample_contents"
 
 spark = SparkSession.builder \
@@ -23,10 +29,13 @@ df = df.withColumn("word_count", size(split(col("content"), ' ')))
 
 # Zlicz łączną liczbę wszystkich słów
 total_count = df.agg(sum("word_count")).collect()[0][0]
-
+total_count.saveAsTextFile(f"gs://{bucket}/{output_file}/")
 result_end = time.time()
 result_time = result_end - result_start
 print(f"Result time: {result_time}")
 
 # Wyświetl wynik
 print(f"Łączna liczba wszystkich słów: {total_count}")
+
+spark.stop()
+
